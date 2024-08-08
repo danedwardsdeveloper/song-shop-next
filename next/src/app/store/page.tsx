888ff17c5721a5ebbdb477ba4f305c9effc575e4
Dan Edwards <daniel.edwards96@yahoo.com>
2024-08-08 20:53:53 +0100

    Add Metadata component

--- next/src/app/store/page.tsx
+++ next/src/app/store/page.tsx
@@ -1,3 +1,5 @@
+import { Metadata } from 'next';
+
 import Hero from './_components/Hero';
 import Offers from './_components/Offers';
 import TrendingSongs from './_components/TrendingSongs';
@@ -8,6 +10,13 @@ import SongTable from './_components/SongTable';
 import { exampleSongs } from './_exampleData/exampleSongs';
 import { exampleTrendingSongs } from './_exampleData/exampleTrendingSongs';
 import { exampleWriters } from './_exampleData/exampleWriters';
+
+import { baseMetadata } from '../_library/metadata';
+
+export const metadata: Metadata = {
+	...baseMetadata,
+	title: 'Store',
+};
 
 export default function Page() {
 	return (
