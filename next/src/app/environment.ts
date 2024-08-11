f3c709830534411bc2f4c48ce8d788ddd8f43b25
Dan Edwards <daniel.edwards96@yahoo.com>
2024-08-11 12:39:54 +0100

    Add array and empty string validation

--- next/src/app/environment.ts
+++ next/src/app/environment.ts
@@ -5,10 +5,30 @@ const nonEmptyStr = makeValidator((value) => {
 	return value;
 });
 
-export const environment = cleanEnv(process.env, {
-	NODE_ENV: str({ choices: ['development', 'production'] }),
-	MAILCHIMP_API_KEY: nonEmptyStr(),
-	ALLOWED_ORIGINS: nonEmptyStr(),
+const commaSeparatedArray = makeValidator((value) => {
+	const baseMessage = `Comma separated string must`;
+	if (typeof value !== 'string') throw new Error(`${baseMessage} be a string`);
+	const trimmedValue = value.trim();
+	if (trimmedValue === '') throw new Error(`${baseMessage} not be empty`);
+
+	const array = trimmedValue
+		.split(',')
+		.map((item) => item.trim())
+		.filter(Boolean);
+	if (array.length === 0)
+		throw new Error(`${baseMessage} must contain at least one value`);
+
+	return array;
 });
 
-export const validateEnvironment = () => environment;
+const envConfig = {
+	NODE_ENV: str({ choices: ['development', 'production'] }),
+	MAILCHIMP_API_KEY: nonEmptyStr(),
+	ALLOWED_ORIGINS: commaSeparatedArray(),
+};
+
+export const environment = cleanEnv(process.env, envConfig);
+
+export const validateEnvironment = () => {
+	cleanEnv(process.env, envConfig);
+};
